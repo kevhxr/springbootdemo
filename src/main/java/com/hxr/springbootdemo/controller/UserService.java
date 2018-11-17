0d1f77a3a77d315a0b2bdec995a7a68acd5d51e9
xiaoran1 <wscqkevin@gmail.com>
2018-11-17 10:52:30 +0800

    first commit

---
+++ src/main/java/com/hxr/springbootdemo/controller/UserService.java
@@ -0,0 +1,18 @@
+package com.hxr.springbootdemo.controller;
+
+import com.hxr.springbootdemo.entity.UserBean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/test")
+public class UserService {
+    @RequestMapping("getuser")
+    public UserBean getUser() {
+        UserBean user = new UserBean();
+        user.setName("test");
+        return user;
+    }
+}
---
+++ src/main/java/com/hxr/springbootdemo/entity/UserBean.java
@@ -0,0 +1,13 @@
+package com.hxr.springbootdemo.entity;
+
+public class UserBean {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
